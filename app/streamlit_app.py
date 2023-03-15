c9867415c157b7d21c9c92d896221fbb0b48643c
Iqra840 <54781026+Iqra840@users.noreply.github.com>
2023-03-15 21:17:42 +0800

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -115,7 +115,7 @@ def check_password():
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["users"]["password"]:
+        if st.session_state["password"] == 'temporarypassword':
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # don't store password
         else:
